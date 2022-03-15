97204960b8eb6faca8bc6375643b336d17a0ca2b
kyocoolcool <kyocoolcool@hotmail.com>
2022-03-15 11:24:04 +0800

    Initial commit

---
+++ src/app/menu/menu.ts
@@ -0,0 +1,29 @@
+import { CoreMenu } from '@core/types'
+
+export const menu: CoreMenu[] = [
+  {
+    id: 'home',
+    title: 'Home',
+    translate: 'MENU.HOME',
+    type: 'item',
+    icon: 'home',
+    url: 'home'
+  },
+  {
+    id: 'sample',
+    title: 'Sample',
+    translate: 'MENU.SAMPLE',
+    type: 'item',
+    icon: 'file',
+    url: 'sample'
+  },
+  {
+    id: 'sample2',
+    title: 'Sample2',
+    translate: 'MENU.SAMPLE2',
+    type: 'item',
+    icon: 'file',
+    url: 'sample'
+  }
+
+]
